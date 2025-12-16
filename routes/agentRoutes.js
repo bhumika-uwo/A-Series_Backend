d0d1ce48739d5c469cc82bad2994733d1db99b72
gurumukhahuja-ux <gurumukhahuja@icloud.com>
2025-12-16 14:58:02 +0530

    solved smtp for email

--- routes/agentRoutes.js
+++ routes/agentRoutes.js
@@ -88,4 +88,4 @@ route.post("/get_my_agents", async (req, res) => {
 
 })
 
-export default route
+export default rout
--- utils/Email.config.js
+++ utils/Email.config.js
@@ -1,12 +1,17 @@
-import nodemailer from "nodemailer"
-const Email = process.env.Email
-const pass = process.env.EMAIL_PASS_KEY
+import nodemailer from "nodemailer";
+
+const EMAIL = process.env.EMAIL;
+const PASS = process.env.EMAIL_PASS_KEY;
+
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // true for port 465, false for other ports
+  secure: false,
   auth: {
-    user: Email,
-    pass: pass,
+    user: EMAIL,
+    pass: PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
